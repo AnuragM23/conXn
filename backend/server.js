92073fafd7fded1dfe7f7ce745886890abcf7864
AnuragM23 <anuragmahato006@gmail.com>
2024-11-08 15:19:33 +0530

    created the auth and message rest api calls

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
-const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cookieParser());
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on http://localhost:${PORT}`);
+});
