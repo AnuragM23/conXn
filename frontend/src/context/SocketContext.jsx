b2d8c7653444f85f8d80aa81e4e5499f00ac2d3c
AnuragM23 <anuragmahato006@gmail.com>
2024-11-16 14:15:06 +0530

    add the deployment link

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:6000/", {
+            const socket = io("https://conxn.onrender.com/", {
                 query:{userId: authUser._id}
             });
             setSocket(socket);
